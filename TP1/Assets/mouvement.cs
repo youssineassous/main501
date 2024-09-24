4c5792ec3a346b55d948a145d91c9e4b33bd3eb1
youssine <youssine.assous@orange.fr>
2024-09-24 14:09:11 +0200

    Premiet commit'

---
+++ TP1/Assets/mouvement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouvement : MonoBehaviour
+{
+    public float jumpForce = 5f;
+    public float speed = 0.01f; 
+    public float speedMultiplier = 3f; 
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+{
+float moveSpeed = speed;
+
+if (Input.GetKey(KeyCode.LeftShift))
+    {
+        moveSpeed *= speedMultiplier; // Applique le multiplicateur de vitesse
+    }
+
+if (Input.GetKey(KeyCode.DownArrow))
+{
+transform.Translate(Vector3.forward * moveSpeed);
+}
+if (Input.GetKey(KeyCode.UpArrow))
+{
+transform.Translate(Vector3.back * moveSpeed);
+}
+if (Input.GetKey(KeyCode.LeftArrow))
+{
+transform.Rotate(Vector3.up, -2);
+}
+if (Input.GetKey(KeyCode.RightArrow))
+{
+transform.Rotate(Vector3.up, 2);
+}
+
+if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+}
+
+private void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+}
